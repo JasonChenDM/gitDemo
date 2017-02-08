d851fc57f248cb63b6054f678446375cd2cc1979
JasonChenDM <545471688@qq.com>
2017-02-08 14:47:37 +0800

    一次测试修改
    就是一次测试修改

--- index.ios.js
+++ index.ios.js
@@ -11,7 +11,7 @@ import {
     Text,
     View
 } from 'react-native';
-
+var my = "heihei";
 var Main = require("./component/Main");
 export default class DouBanDemo extends Component {
     render() {
